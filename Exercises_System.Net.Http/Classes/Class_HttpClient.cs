6d19c28c8f1e8126f916f5b98602588f854647dd
Daniel Vega Caro <daniel.vega@keylogistics.cl>
2024-02-22 11:17:21 -0300

    HttpClient: GET

---
+++ Exercises_System.Net.Http/Classes/Class_HttpClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Exercises_System.Net.Http.Classes
+{
+    public class Class_HttpClient
+    {
+        public async Task HttpGetRequest()
+        {
+            // Create an HTTP client
+            // Crear un cliente HTTP
+            using (var client = new HttpClient())
+            {
+                // Make a GET request to a URL
+                // Hacer una solicitud GET a una URL
+                var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+
+                // Check if the request was successful
+                // Verificar si la solicitud fué exitosda
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read the content of the response as a string
+                    // Leer el contenido de la respuesta como una cadena
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(content);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+        }
+
+    }
+}
---
+++ Exercises_System.Net.Http/Program.cs
@@ -0,0 +1,44 @@
+﻿using Exercises_System.Net.Http.Classes;
+using System;
+using System.Threading.Tasks;
+
+namespace Exercises_System.Net.Http
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Choose an option to see how it works:");
+            Console.WriteLine("Elija una opción para ver su funcionamiento:");
+            Console.WriteLine("");
+            Console.WriteLine("1. HttpClient: GET");
+            Console.WriteLine("2. Opción 2");
+            Console.WriteLine("3. Opción 3");
+            Console.WriteLine("4. Salir");
+
+            Console.WriteLine("");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    var myHttpClient = new Class_HttpClient();
+                    await myHttpClient.HttpGetRequest();
+                    break;
+                case "2":
+                    Console.WriteLine("Ha elegido la Opción 2");
+                    break;
+                case "3":
+                    Console.WriteLine("Ha elegido la Opción 3");
+                    break;
+                case "4":
+                    Console.WriteLine("Saliendo...");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        }
+    }
+}
