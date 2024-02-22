a549c231150b1ecec68a2067227a98c4a9418672
Daniel Vega Caro <daniel.vega@keylogistics.cl>
2024-02-22 14:52:42 -0300

    StringContent

---
+++ Exercises_System.Net.Http/Classes/Class_StringContent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Exercises_System.Net.Http.Classes
+{
+    internal class Class_StringContent
+    {
+        public async Task CopyRequestContent()
+        {
+            // Create an HTTP client
+            // Crear un cliente HTTP
+            var content = new StringContent("{\"key\": \"value\"}", System.Text.Encoding.UTF8, "application/json");
+
+            // Create an output stream
+            // Crear un flujo de salida
+            using (var outputStream = new MemoryStream())
+            {
+                // Copy request content to output stream
+                // Copiar el contenido de la solicitud al flujo de sallida
+                await content.CopyToAsync(outputStream);
+
+                // Convert the output stream to a string and display it in the console
+                // Convertir el flujo de salida a una cadena y mostrarla en la consola
+                var contentString = System.Text.Encoding.UTF8.GetString(outputStream.ToArray());
+                Console.WriteLine(contentString);
+            }
+        }
+    }
+}
--- Exercises_System.Net.Http/Program.cs
+++ Exercises_System.Net.Http/Program.cs
@@ -11,9 +11,9 @@ namespace Exercises_System.Net.Http
             Console.WriteLine("Choose an option to see how it works:");
             Console.WriteLine("Elija una opción para ver su funcionamiento:");
             Console.WriteLine("");
-            Console.WriteLine("1. HttpClient: GET");
-            Console.WriteLine("2. HttpClient: POST");
-            Console.WriteLine("3. Opción 3");
+            Console.WriteLine("1. HttpClient - GetAsync()");
+            Console.WriteLine("2. HttpClient - PostAsync()");
+            Console.WriteLine("3. StringContent - CopyToAsync()");
             Console.WriteLine("4. Salir");
 
             Console.WriteLine("");
@@ -21,6 +21,7 @@ namespace Exercises_System.Net.Http
             string opcion = Console.ReadLine();
 
             var myHttpClient = new Class_HttpClient();
+            var myStringContent = new Class_StringContent();
 
             switch (opcion)
             {
@@ -31,7 +32,7 @@ namespace Exercises_System.Net.Http
                     await myHttpClient.HttpPostRequest();
                     break;
                 case "3":
-                    Console.WriteLine("Ha elegido la Opción 3");
+                    await myStringContent.CopyRequestContent();
                     break;
                 case "4":
                     Console.WriteLine("Saliendo...");
